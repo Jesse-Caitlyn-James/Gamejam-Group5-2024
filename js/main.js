57b536c0b2b6e0913f7f724e419a129d23e8a1b2
Jesse Caitlyn James <jesse.caitlyn.james20@gmail.com>
2024-10-27 11:14:50 +1000

    Button Works

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 /**
  * 
  * @param {*} x X Position
@@ -8,28 +7,27 @@
  * @param {*} callback Callback Function for Click Event
 */    
 class gameButton {
-    constructor(x,y,w,h,callback = "hi"){
+    constructor(x,y,w,h,text = "",callback = console.log){
         this.sprite = new buttonGroup.Sprite();
         this.sprite.x = x;
         this.sprite.y = y;
         this.sprite.w = w;
         this.sprite.h = h;
+        this.sprite.text = text;
+        this.sprite.textSize = (w + h)/4;
         this.sprite.collider = "none";
         this.sprite.gameButton = this;
         this.callback = callback;
     }
 }
 
-
 let buttonGroup
-
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
     buttonGroup = new Group();
-    test = new gameButton(windowWidth/2, windowHeight/2, 100, 100);
+    test = new gameButton(windowWidth/2, windowHeight/2, 100, 100, "butt", drawPent);
 }
-
 
 function draw(){
     background(220);
@@ -42,8 +40,12 @@ function draw(){
             let corner2y = buttonGroup[i].y + buttonGroup[i].h/2;
 
             if(mouse.x > corner1x && mouse.x < corner2x && mouse.y > corner1y && mouse.y < corner2y){
-                console.log(buttonGroup[i].gameButton.callback);
+                buttonGroup[i].gameButton.callback();
             }
         }
     }
+}
+
+function drawPent(){
+    new Sprite(windowWidth/2, windowHeight/2, 10, 'pentagon');
 }
