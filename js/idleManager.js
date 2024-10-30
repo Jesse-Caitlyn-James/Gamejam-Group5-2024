2cb354031f98cf7698210ee4fcbf65f6618216f3
Jesse Caitlyn James <jesse.caitlyn.james20@gmail.com>
2024-10-30 23:05:05 +1000

    Resources spawn now

--- js/idleManager.js
+++ js/idleManager.js
@@ -1,14 +1,17 @@
-class IdleManager{
-    constructor(){
+class IdleManager {
+    constructor() {
         this.resourceGroup = new Group();
         this.resourceGathererGroup = new Group();
         this.gatherTarget = this.createTarget();
 
+        // Remeber each second is 60 frames so Default: 30 = 1/2 sec, 60 = 1 sec
+        this.gatherSpeed = 30;
+
         // UI buttons gettin made
-        this.gathererButton = new GameButton(110, windowHeight-50, 200, 80, "Gatherer", this.createResourceGatherer);
+        this.gathererButton = new GameButton(110, windowHeight - 50, 200, 80, "Gatherer", this.createResourceGatherer);
     }
-    
-    createTarget(){
+
+    createTarget() {
         let target = new Sprite();
         target.d = 100;
         target.image = imgTarget;
@@ -18,29 +21,26 @@ class IdleManager{
 
         return target;
     }
-    
-    createResourceGatherer(){
+
+    createResourceGatherer() {
         factory.makeUnit("gatherer", idleManager.resourceGathererGroup);
     }
 
-    createResource(){
-        // Works similar to the above - factory code not implemented yet
-        // factory.makeUnit("resource", idleManager.resourceGroup);
+    createResource() {
+        factory.makeUnit("resource", idleManager.resourceGroup);
     }
 
-    checkResources(){
-        // Checks conditions for spawning new resources
-        // different function cause we can make this more complex later
-        if(this.resourceGroup.length < 20){
+    checkResources() {
+        if (this.resourceGroup.length < 20) {
             this.createResource();
         }
     }
-    
-    idleUpdate(){
+
+    idleUpdate() {
         // Following is the target and gatherer movement code
         this.moveTarget();
 
-        for(let i = 0; i < this.resourceGathererGroup.length; i++){
+        for (let i = 0; i < this.resourceGathererGroup.length; i++) {
             this.resourceGathererGroup[i].moveTo(this.gatherTarget, 0.5);
         }
 
@@ -49,37 +49,38 @@ class IdleManager{
         this.harvestResources();
     }
 
-    moveTarget(){
-        if(mouse.pressing()){
+    moveTarget() {
+        if (mouse.pressing()) {
             let targetDist = dist(mouse.x, mouse.y, this.gatherTarget.x, this.gatherTarget.y);
-            if (targetDist <= 50){
+            if (targetDist <= 50) {
                 this.gatherTarget.picked = true;
             }
         }
-        else{
+        else {
             this.gatherTarget.picked = false;
         }
 
-        if(this.gatherTarget.picked){
+        if (this.gatherTarget.picked) {
             this.gatherTarget.x = mouse.x;
             this.gatherTarget.y = mouse.y;
         }
     }
 
-    harvestResources(){
-        for(let i = 0; i < this.resourceGroup; i++){
-            let resource = this.resourceGroup[i];
-            for(let j = 0; j < this.resourceGathererGroup; j++){
-                let gatherer = this.resourceGathererGroup[j];
-                let gatherDist = dist(resource.x, resource.y, gatherer.x, gatherer.y);
-                if(gatherDist < 20){
-                    resource.health--;
-                    if (resource.health == 0){
-                        // Code to add resource to player's resource pool
-                        // Or we could make the gatherer hold the resource to take back to base?
-                        // I have the code for this^ somewhere
-                        // Code to add this resource to removal list
-                        // Pretty sure p5 removes immediately so we can't remove it during the loop
+    harvestResources() {
+        if (frameCount % this.gatherSpeed == 0) {
+            for (let i = 0; i < this.resourceGroup.length; i++) {
+                let resource = this.resourceGroup[i];
+                for (let j = 0; j < this.resourceGathererGroup.length; j++) {
+                    let gatherer = this.resourceGathererGroup[j];
+                    let gatherDist = dist(resource.x, resource.y, gatherer.x, gatherer.y);
+                    if (gatherDist < 20) {
+                        resource.health--;
+                        if (resource.health == 0) {
+                            // Code to add resource to player's resource pool
+                            // Or we could make the gatherer hold the resource to take back to base?
+                            // I have the code for this^ somewhere
+                            resource.remove();
+                        }
                     }
                 }
             }
--- js/unitFactory.js
+++ js/unitFactory.js
@@ -8,6 +8,9 @@ class UnitFactory{
             case "gatherer":
                 this.makeGatherer(group);
                 break;
+            case "resource":
+                this.makeResources(group);
+                break;
             }
     }
     
@@ -23,5 +26,21 @@ class UnitFactory{
         unit.y = windowHeight/2;
         unit.w = 10;
         unit.h = 10;
+    }
+
+    makeResources(group){
+        let unit;
+        if (group != null){
+            unit = new group.Sprite();
+        }
+        else{
+            unit = new Sprite();
+        }
+        unit.x = random(windowWidth);
+        unit.y = random(windowHeight-150);
+        unit.w = 30;
+        unit.h = 30;
+        unit.collider = "none";
+        unit.health = 20;
     }
 }
